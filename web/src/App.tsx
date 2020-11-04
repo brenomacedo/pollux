4c3cea1c86fde524546a729d0b8e1803a36d8199
brenosekiro <b88660501@gmail.com>
2020-11-04 07:53:51 -0300

    loginScreen

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import './App.css'
+import { createGlobalStyle } from 'styled-components'
+import Login from './pages/Login'
+
+const GlobalStyle = createGlobalStyle`
+    :root {
+        --RW: 'Raleway', sans-serif;
+    }
+
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+`
 
 function App() {
     return (
-        <div className="App">
-            
-        </div>
+        <>
+            <GlobalStyle />
+            <Login />
+        </>
     )
 }
 
---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,173 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { FiEye, FiEyeOff, FiCheck } from 'react-icons/fi'
+
+interface MarkerProps {
+    active: boolean
+}
+
+const Container = styled.div`
+    min-height: 100vh;
+    display: flex;
+`
+
+const LeftSide = styled.div`
+    min-height: 100vh;
+    flex: 1;
+    background-color: blue;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-image: linear-gradient(135deg, transparent 0%, transparent 26%,rgba(19, 135, 203,0.6) 26%, rgba(19, 135, 203,0.6) 72%,transparent 72%, transparent 82%,rgba(6, 156, 244,0.6) 82%, rgba(6, 156, 244,0.6) 100%),linear-gradient(45deg, transparent 0%, transparent 4%,rgb(19, 135, 203) 4%, rgb(19, 135, 203) 22%,rgb(45, 92, 120) 22%, rgb(45, 92, 120) 60%,transparent 60%, transparent 100%),linear-gradient(90deg, rgb(255,255,255),rgb(255,255,255));
+
+    img {
+        width: 120px;
+        height: 120px;
+    }
+
+    p {
+        font-family: var(--RW);
+        font-size: 20px;
+        margin-top: 20px;
+        color: white;
+        font-weight: bold;
+    }
+`
+
+const RightSide = styled.div`
+    width: 500px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background-color: #e6fbff;
+
+    form {
+        width: 300px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+    p {
+        font-family: var(--RW);
+        margin-top: 10px;
+        user-select: none;
+        cursor: pointer;
+    }
+
+    h1 {
+        font-family: var(--RW);
+    }
+
+    input {
+        width: 100%;
+        margin-top: 10px;
+        height: 50px;
+        outline: none;
+        font-family: var(--RW);
+        border-radius: 8px;
+        border: 1px solid #ccc;
+        padding-left: 12px;
+        font-size: 20px;
+    }
+
+    button {
+        background-color: #00ffdd;
+        outline: none;
+        border: 0;
+        width: 100%;
+        margin-top: 10px;
+        height: 50px;
+        border-radius: 8px;
+        cursor: pointer;
+        font-family: var(--RW);
+        color: white;
+        font-size: 20px;
+        user-select: none;
+    }
+
+    button:hover {
+        background-color: #00ffef;
+    }
+`
+
+const LoginOptions = styled.div`
+    width: 100%;
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    font-family: var(--RW);
+
+    p {
+        user-select: none;
+        cursor: initial;
+    }
+`
+
+const Marker = styled.div<MarkerProps>`
+    background-color: ${props => props.active ? 'lightgreen' : 'white'};
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    cursor: pointer;
+    border: 1px solid #ccc;
+`
+
+const IPassContainer = styled.div`
+    width: 100%;
+    position: relative;
+`
+
+const Icon = styled.div`
+    position: absolute;
+    right: 20px;
+    top: 26px;
+    cursor: pointer;
+`
+
+const Login = () => {
+
+    const [remember, setRemember] = useState(false)
+    const [passwordVisible, setPasswordVisible] = useState(false)
+
+    return (
+        <Container>
+            <LeftSide>
+                <img src="/star.svg" alt="logo"/>
+                <p>Pollux</p>
+            </LeftSide>
+            <RightSide>
+                <form onSubmit={() => {}}>
+                    <h1>Login</h1>
+                    <input placeholder="Your email" type="email"/>
+                    <IPassContainer>
+                        <input placeholder="Your password"
+                        type={passwordVisible ? "text" : "password"}/>
+                        {passwordVisible ? (<Icon onClick={() => setPasswordVisible(false)}>
+                            <FiEye size={20} color='black' />
+                        </Icon>) : (<Icon onClick={() => setPasswordVisible(true)}>
+                            <FiEyeOff size={20} color='black' />
+                        </Icon>)}
+                    </IPassContainer>
+                    <LoginOptions>
+                        <Marker active={remember} ><FiCheck size={15} color='white'
+                        onClick={() => setRemember(!remember)} /></Marker>
+                        <p>Remember-me</p>
+                    </LoginOptions>
+                    <button type="submit">Login</button>
+                    <p>I forgot my password</p>
+                    <p>I don't have an account</p>
+                </form>
+            </RightSide>
+        </Container>
+    )    
+}
+
+export default Login
